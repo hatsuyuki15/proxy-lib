58ad68b357ca9a599e94031b401a4ea0495a9bdf
hatsuyuki15 <hatsuyuki15@gmail.com>
2016-11-04 20:44:10 +0700

    (balancer tie-breaker) prefer least recently used one of all pipelines with the same number of active connections

--- src/main/java/org/hatsuyuki/proxy/BalanceSplitter.java
+++ src/main/java/org/hatsuyuki/proxy/BalanceSplitter.java
@@ -1,47 +1,56 @@
 package org.hatsuyuki.proxy;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Hatsuyuki.
  */
 public class BalanceSplitter extends Splitter{
-    private Map<Pipeline, Integer> activeConnectionsPerPipeline = new HashMap<>();
+    private List<Pipeline> pipelines = new ArrayList<>();
+    private Map<Pipeline, Integer> activeConnectionCountMap = new HashMap<>();
+    private Map<Pipeline, Date> lastUsedTimeMap = new HashMap<>();
 
     @Override
     public Response forward(Request request) throws IOException {
         Pipeline pipeline;
-        synchronized (activeConnectionsPerPipeline) {
+        synchronized (activeConnectionCountMap) {
             pipeline = selectPipeline(request);
-            int activeConnection = activeConnectionsPerPipeline.get(pipeline);
+            int activeConnection = activeConnectionCountMap.get(pipeline);
             activeConnection += 1;
-            activeConnectionsPerPipeline.put(pipeline, activeConnection);
+            activeConnectionCountMap.put(pipeline, activeConnection);
+            lastUsedTimeMap.put(pipeline, new Date());
         }
         Response response = pipeline.forward(request);
-        synchronized (activeConnectionsPerPipeline) {
-            int activeConnection = activeConnectionsPerPipeline.get(pipeline);
+        synchronized (activeConnectionCountMap) {
+            int activeConnection = activeConnectionCountMap.get(pipeline);
             activeConnection -= 1;
-            activeConnectionsPerPipeline.put(pipeline, activeConnection);
+            activeConnectionCountMap.put(pipeline, activeConnection);
         }
         return response;
     }
 
     @Override
     protected Pipeline selectPipeline(Request request) {
-        int minActiveConnection = Collections.min(activeConnectionsPerPipeline.values());
-        for (Pipeline pipeline: activeConnectionsPerPipeline.keySet()) {
-            int activeConnection = activeConnectionsPerPipeline.get(pipeline);
-            if (activeConnection == minActiveConnection) {
-                return pipeline;
-            }
-        }
-        return null;
+        return pipelines.stream()
+                .min((pipe1, pipe2) -> {
+                    int activeConnectionsOfPipe1 = activeConnectionCountMap.get(pipe1);
+                    int activeConnectionsOfPipe2 = activeConnectionCountMap.get(pipe2);
+                    if (activeConnectionsOfPipe1 != activeConnectionsOfPipe2) {
+                        // find one with least connections
+                        return Integer.compare(activeConnectionsOfPipe1, activeConnectionsOfPipe2);
+                    } else {
+                        // tie-breaker: find one with oldest usage
+                        Date lastUsedTimeOfPipe1 = lastUsedTimeMap.get(pipe1);
+                        Date lastUsedTimeOfPipe2 = lastUsedTimeMap.get(pipe2);
+                        return Long.compare(lastUsedTimeOfPipe1.getTime(), lastUsedTimeOfPipe2.getTime());
+                    }
+                }).get();
     }
 
     public void addPipeline(Pipeline pipeline) {
-        activeConnectionsPerPipeline.put(pipeline, 0);
+        pipelines.add(pipeline);
+        activeConnectionCountMap.put(pipeline, 0);
+        lastUsedTimeMap.put(pipeline, new Date());
     }
 }
