e1d7e7aa4d5f96459bddb29f65c0498af916ba38
hatsuyuki15 <hatsuyuki15@gmail.com>
2017-02-08 15:21:55 +0700

    - remove support for SynchronizedThrottler - allow specify max timeout for throttler

--- src/main/java/org/hatsuyuki/proxy/Throttler.java
+++ src/main/java/org/hatsuyuki/proxy/Throttler.java
@@ -1,10 +1,63 @@
 package org.hatsuyuki.proxy;
 
+import com.google.common.collect.EvictingQueue;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Hatsuyuki on 12/15/2016.
  */
-public abstract class Throttler extends Pipeline {
-    public Throttler(Pipeline nextPipeline) {
+public class Throttler extends Pipeline {
+    private EvictingQueue<Date> requestDateHistory;
+    private long interval;
+    private long timeout;
+
+    public Throttler(Pipeline nextPipeline, int maxRequestsPerInterval, long interval, long timeout, TimeUnit timeUnit) {
         super(nextPipeline);
+        this.timeout = TimeUnit.MILLISECONDS.convert(timeout, timeUnit);
+        this.interval = TimeUnit.MILLISECONDS.convert(interval, timeUnit);
+        this.requestDateHistory = EvictingQueue.create(maxRequestsPerInterval); // only keep k (=maxRequestsPerInterval) most recent request dates
+    }
+
+    public Throttler(Pipeline nextPipeline, int maxRequestsPerInterval, long interval, TimeUnit timeUnit) {
+        this(nextPipeline, maxRequestsPerInterval, interval, Long.MAX_VALUE, timeUnit);
+    }
+
+    @Override
+    public Response forward(Request request) throws IOException {
+        long waitTime;
+        synchronized (this) {
+            // compute waitTime
+            Date now = new Date();
+            Date oldestRequestDate = requestDateHistory.peek();
+            if (oldestRequestDate == null) {
+                waitTime = 0;
+            } else {
+                long elapsedTime = now.getTime() - oldestRequestDate.getTime();
+                waitTime = elapsedTime > interval
+                         ? 0
+                         : interval - elapsedTime;
+            }
+
+            // drop if waitTime exceed limit
+            if (waitTime > timeout) {
+                throw new IOException("Exceed timeout. Request was dropped by throttler");
+            }
+
+            // schedule executed time
+            Date scheduledDate = new Date(now.getTime() + waitTime);
+            requestDateHistory.offer(scheduledDate);
+        }
+
+        // stand by till scheduled time
+        if (waitTime > 0) {
+            try {
+                Thread.sleep(waitTime);
+            } catch (InterruptedException ignored) {}
+        }
+
+        return nextPipeline.forward(request);
     }
 }
