9d44fae540e3630acbd05857b9c421112aebcd62
hatsuyuki15 <hatsuyuki15@gmail.com>
2017-11-22 13:54:01 +0700

    change DummyProxy -> LocalProxy to support custom pipeline for local request system

--- src/main/java/org/hatsuyuki/proxy/LocalProxy.java
+++ src/main/java/org/hatsuyuki/proxy/LocalProxy.java
@@ -5,8 +5,16 @@ import org.jsoup.Connection;
 /**
  * Created by Hatsuyuki on 12/27/2016.
  */
-public class DummyProxy extends Proxy {
-    private Pipeline pipeline = new LocalRequester();
+public class LocalProxy extends Proxy {
+    private Pipeline pipeline;
+
+    public LocalProxy() {
+        this.pipeline = new LocalRequester();
+    }
+
+    public LocalProxy(Pipeline pipeline) {
+        this.pipeline = pipeline;
+    }
 
     @Override
     public Response request(Connection jsoupConnection) throws Exception {
